033e51fa25f1235b5fa1ed4c89132edbdedd0415
emmanuel_okugo <okugoemma@gmail.com>
2024-04-05 13:38:38 +0000

    formated my float to .2f

--- 0x00-python-hello_world/them.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-try:
-    Number = int(input("Enter a number: "))
-    print(f"{Number} Battery street ")
-except ValueError:
-    raise ValueError("Invalid input: Please enter a valid number")
-
