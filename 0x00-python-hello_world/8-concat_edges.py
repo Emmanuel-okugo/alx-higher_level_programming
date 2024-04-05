396d6772fcf1284cd19c33626404e081546978e4
emmanuel_okugo <okugoemma@gmail.com>
2024-04-05 23:45:35 +0000

    python script that uses slices and + appendint to print a string

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented\
+        language that combines remarkable power with very clear syntax"
+str = str[39:54] + " " + 'programming' + " " + 'with' + " " + 'python'
+print(str)
