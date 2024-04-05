18edf81ca43ddd1795e402cc32db2b31c5464c1b
emmanuel_okugo <okugoemma@gmail.com>
2024-04-05 13:10:06 +0000

    update

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-try:
-    number = '98'
-    print(f"{number} Battery street")
-except ValueError:
-    raise ValueError("Unknown format code 'd' for object of type 'str'")
+number = 98
+print(f"{number:d} Battery street")
